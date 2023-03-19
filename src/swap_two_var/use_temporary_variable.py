1a95198ee3a63d6b8170bbadc1f61fbd469f6dee
Charles Tsai <charles_tsai@symphox.net>
2023-03-19 15:34:55 +0800

    add swap two variables example

---
+++ src/swap_two_var/use_temporary_variable.py
@@ -0,0 +1,9 @@
+x = 5
+y = 10
+
+temp = x
+x = y
+y = temp
+
+print(f'x = {x}') # x = 10
+print(f'y = {y}') # y = 5
---
+++ src/swap_two_var/without_temporary_variable.py
@@ -0,0 +1,7 @@
+x = 5
+y = 10
+
+x, y = y, x
+
+print(f'x = {x}') # x = 10
+print(f'y = {y}') # y = 5
